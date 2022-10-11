a8368e7ad0814600d21c1981accea4f15b86d24e
RIKO910 <tarikulislamriko910@gmail.com>
2022-10-11 17:32:48 +0600

    set environment

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,16 +1,41 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import './Register.css'
+import { useNavigate } from 'react-router-dom';
 const Register = () => {
+    
+    const navigate =useNavigate()
+    const emailRef = useRef('');
+    const passwordRef =useRef('');
+    const nameRef=useRef('');
+
+
+
+const navigateLogin =event =>{
+  navigate('/login');
+  console.log(event.target.value)
+}
+const handleRegister =event =>{
+    event.preventDefault();
+    const email =emailRef.current.value;
+    const password =passwordRef.current.value;
+    const name =nameRef.current.value;
+    console.log(name,password,email)
+}
     return (
-        <div>
+        <div className='containerRegister'>
             <h2>Please Register</h2>
-            <form>
-                <input type="text" name='name' id='' placeholder='your name' />
+            <form onSubmit={handleRegister} className='form'>
+                <input ref={nameRef} type="text" name='name' id='' placeholder='your name' />
                 <br />
-                <input type="text" name='email' id='' placeholder='email' required/>
+                <input ref={emailRef} type="email" name='email' id='' placeholder='email' required/>
                 <br />
-                <input type="text" name='email' id='' placeholder='email' required/>
+                <input ref={passwordRef} type="password" name='password' id='' placeholder='password' required/>
+                <br />
+                <input className='submit' type="submit" value="Register" />
             </form>
+            
+            <p>Already have an account <span role="button" onClick={navigateLogin} className='text-danger'>Please Login</span></p>
+
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import{getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2rcGlIaYvELwHSQ862XgOYcpw53oEYD0",
-  authDomain: "car-services-410c9.firebaseapp.com",
-  projectId: "car-services-410c9",
-  storageBucket: "car-services-410c9.appspot.com",
-  messagingSenderId: "593951601206",
-  appId: "1:593951601206:web:09bf11f26a9a2ce233de97"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
